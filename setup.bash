13dad0bda082ca91873c0a7bb3501f64eaadcc67
Bas Maas <bas.maas@sogeti.com>
2020-05-24 00:01:22 +0200

    exec in running container should work now

--- setup.bash
+++ setup.bash
@@ -16,8 +16,11 @@ if [ ! -x "$(command -v docker-compose)" ]; then
     sudo apt install docker-compose
     docker-compose version
 fi
-sudo docker-compose run -d mqtt
-sudo docker exec -t mqtt echo "$1:$2" >> ./passwd && mosquitto_passwd -U ./passwd
+sudo docker-compose run -d --name mqtt mqtt
+sleep 3
+sudo docker exec "mqtt" sh -c "echo '$1:$2' >> /passwd"
+sudo docker exec "mqtt" sh -c "mosquitto_passwd -U ./passwd"
+sudo docker exec -t mqtt echo "bas:asdf" >> ./passwd && mosquitto_passwd -U ./passwd
 sudo docker-compose down
 sudo docker-compose up -d
 sudo docker ps -a
